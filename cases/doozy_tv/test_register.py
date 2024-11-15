a9f15694b3fd324a0f88182d003078143cfa967b
frankie <frankie.hu@ziyiltd.com>
2024-11-15 16:53:37 +0800

    添加注册用例

--- cases/doozy_tv/test_register.py
+++ cases/doozy_tv/test_register.py
@@ -4,19 +4,21 @@
 """
  @author      :  Frankie
  @description :  
- @time        :    10:45
+ @time        :    14:59
 """
 from base.app_ui.android_Project_client import Android_Project_Client
 from page_objects.doozy_tv.pages.loginPage import LoginPage
+from page_objects.doozy_tv.pages.registerPage import RegisterPage
 from common.logger.logTool import logger
 import pytest
 
 
-class TestLogin:
+class TestRegister:
     def setup_class(self):
         """复用 Android_Project_Client 实例，只初始化一次"""
         self._demoProjectClient = Android_Project_Client()  # 此处不会重新初始化
         self._loginPage = LoginPage(self._demoProjectClient.appOperator)
+        self._registerPage = RegisterPage(self._demoProjectClient.appOperator)
         self._appOperator = self._demoProjectClient.appOperator
 
     @pytest.fixture
@@ -25,19 +27,13 @@ class TestLogin:
         yield self.fixture_test
         logger.info('\n...... end ......')
 
-    @pytest.mark.run(order=5)
+    @pytest.mark.run(order=4)
     # @pytest.mark.skipif(reason='跳过此用例')
-    def test_login_email(self, fixture_test):
+    def test_register_email(self, fixture_test):
         self._appOperator.restart_app()
         self._loginPage.click_profile_btn()
         self._loginPage.click_login_btn()
-        self._loginPage.click_login_with_btn()
-        self._loginPage.switch_to_login_with_email()
-        self._loginPage.input_login_email()
-        self._loginPage.input_login_email_password()
-        self._loginPage.click_login()
-        self._loginPage.click_profile_btn()
-        self._loginPage.check_login_success()
+
 
     def teardown_class(self):
         logger.info('TestStartUpADPage 用例执行结束，保留应用状态')
--- page_objects/doozy_tv/pages/registerPage.py
+++ page_objects/doozy_tv/pages/registerPage.py
@@ -6,3 +6,48 @@
  @description :  
  @time        :    13:45
 """
+from common.logger.logTool import logger
+from common.yamlTool import YamlTool
+from common.appium.remoteControlActions import RemoteControlActions
+from page_objects.doozy_tv.elements.registerPageElements import RegisterPageElements
+from page_objects.doozy_tv.elements.topMenuElements import TopMenuElements
+from page_objects.doozy_tv.elements.profilePageElements import ProfilePageElements
+from page_objects.doozy_tv.elements.loginMethodPageElements import LoginMethodPageElements
+
+
+class RegisterPage:
+    def __init__(self, _appOperator):
+        self._appOperator = _appOperator
+        self._remoteControl = RemoteControlActions(self._appOperator.getDriver())
+        self.registerPageElements = RegisterPageElements()
+        self.topMenuElements = TopMenuElements()
+        self.profilePageElements = ProfilePageElements()
+        self.loginMethodPageElements = LoginMethodPageElements()
+
+    def click_back_btn(self):
+        self._remoteControl.press_back()
+        logger.info('Click back button')
+
+    def move_to_register_email_element(self):
+        if self._appOperator.is_displayed(self.registerPageElements.register_email_element):
+            self._appOperator.move_cursor_to_element(self.registerPageElements.register_email_element)
+            if self._appOperator.is_selected(self.registerPageElements.register_email_element):
+                logger.info('Move to register email element')
+                self._appOperator.get_screenshot('Move to register email element')
+
+    def move_to_email_input_element(self):
+        if self._appOperator.is_displayed(self.registerPageElements.register_email_input_text):
+            self._appOperator.move_cursor_to_element(self.registerPageElements.register_email_input_text)
+            logger.info('Move to email input element')
+            self._appOperator.get_screenshot('Move to email input element')
+            if self._appOperator.is_selected(self.registerPageElements.register_email_input_text):
+                logger.info('Email input element is selected')
+
+    def input_register_email(self, email):
+        self._appOperator.input_text(self.registerPageElements.register_email_input_text, email)
+
+    def move_to_register_phone_element(self):
+        if self._appOperator.is_displayed(self.registerPageElements.register_phone_element):
+            self._appOperator.move_cursor_to_element(self.registerPageElements.register_phone_element)
+            logger.info('Move to register phone element')
+            self._appOperator.get_screenshot('Move to register phone element')
